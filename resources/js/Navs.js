d46475ec502e2cb7fb8ce145fe906246c087d979
Farhan izzaz <farhanizzaz@gmail.com>
2022-09-08 21:56:08 +0700

    Update Navs.js

--- resources/js/Navs.js
+++ resources/js/Navs.js
@@ -47,7 +47,7 @@ function Navs() {
         },
     
     })    
-    axios.defaults.baseURL = "https://cryptic-headland-94862.herokuapp.com/https://example.com/";
+    axios.defaults.baseURL = "https://cryptic-headland-94862.herokuapp.com/http://kekean.pusproset.site/";
     axios.defaults.headers.post['Accept'] = "application/json";
     axios.defaults.headers.post['Content-Type'] = "application/json";
     axios.defaults.withCredentials = true; 
