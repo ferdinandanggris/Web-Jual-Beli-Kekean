59deedd7a73cfbd3536f75d1ba95b7c4f5c0d512
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-02 21:38:20 +0700

    Update ApiAdminMiddleware.php

--- app/Http/Middleware/ApiAdminMiddleware.php
+++ app/Http/Middleware/ApiAdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiAdminMiddleware
 {
@@ -16,6 +17,20 @@ class ApiAdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check()) {
+            if(auth()->user()->tokenCan('server:admin')) {
+                return $next($request);
+            } else {
+                return response()->json([
+                    'message' => 'Access Denied!',
+                ], 403);
+            }
+
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Please Login First',
+            ]);
+        }
     }
 }
