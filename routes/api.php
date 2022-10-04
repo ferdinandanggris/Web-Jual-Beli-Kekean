ce5c0defd96f7d064dff46822d525c87c624006c
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-04 18:52:02 +0700

    changed cart cont

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CartController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\CartController;
 
 /*
 |--------------------------------------------------------------------------
