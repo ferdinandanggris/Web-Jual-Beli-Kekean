5bcb4eed6ff32a61ca21d75108cda397ebcf8316
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-04 20:07:46 +0700

    Update 2022_10_04_125249_create_new_column_keranjangs.php

--- database/migrations/2022_10_04_125249_create_new_column_keranjangs.php
+++ database/migrations/2022_10_04_125249_create_new_column_keranjangs.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateKeranjangsTable extends Migration
+class CreateNewColumnKeranjang extends Migration
 {
     /**
      * Run the migrations.
