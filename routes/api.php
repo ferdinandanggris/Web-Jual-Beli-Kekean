0d0ee84e48595460a037560a606f1686dff5d944
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-04 18:47:59 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\CartController;
+use App\Http\Controllers\API\CartController;
 
 /*
 |--------------------------------------------------------------------------
