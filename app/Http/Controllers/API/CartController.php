854b367a085f9d1bccd8b8ce1ba1ec694d267629
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-05 22:07:44 +0700

    Update CartController.php

--- app/Http/Controllers/API/CartController.php
+++ app/Http/Controllers/API/CartController.php
@@ -99,26 +99,28 @@ class CartController extends Controller
         }
     }
 
-    public function deleteCartItem($cart_id) {
-    if (auth('sanctum')->check()) {
-        $user_id = auth('sanctum')->user()->id;
-        $cartItem = Keranjang::where('id', $cart_id)->where('user_id', $user_id)->first();
-        if($cartItem) {
-            $cartItem->delete();
-            return response()->json([
-                'status' => 200,
-                'message' => 'Item berhasil dihapus',
-            ]);
+    public function deleteCartItem($cart_id)
+    {
+        if (auth('sanctum')->check()) {
+            $user_id = auth('sanctum')->user()->id;
+            $cartItem = Keranjang::where('id', $cart_id)->where('user_id', $user_id)->first();
+            if ($cartItem) {
+                $cartItem->delete();
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Item berhasil dihapus',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Item tidak ditemukan',
+                ]);
+            }
         } else {
             return response()->json([
                 'status' => 401,
-                'message' => 'Item tidak ditemukan',
+                'message' => 'Login untuk mengubah keranjang',
             ]);
         }
-    } else {
-        return response()->json([
-            'status' => 401,
-            'message' => 'Login untuk mengubah keranjang',
-        ]);
     }
 }
