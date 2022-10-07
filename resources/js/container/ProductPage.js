6e64cf740566d431cb8eecd06ec0de0725446735
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-08 00:12:45 +0700

    fixed carousel

--- resources/js/container/ProductPage.js
+++ resources/js/container/ProductPage.js
@@ -137,7 +137,7 @@ export default function ProductPage(props) {
                                         component="img"
                                         src={`../catalog/${currentProduct[0].image_detail1}`}
                                     />
-                                    <Box
+                                    {!!currentProduct[0].image_detail2 ? <Box
                                         sx={{
                                             width: "538px",
                                             height: "400px",
@@ -145,7 +145,16 @@ export default function ProductPage(props) {
                                         }}
                                         component="img"
                                         src={`../catalog/${currentProduct[0].image_detail2}`}
-                                    />
+                                    /> : ''}
+                                    {!!currentProduct[0].image_detail3 ? <Box
+                                        sx={{
+                                            width: "538px",
+                                            height: "400px",
+                                            objectFit: "cover",
+                                        }}
+                                        component="img"
+                                        src={`../catalog/${currentProduct[0].image_detail3}`}
+                                    /> : ''}
                                 </Carousel>
                                 // <Box
                                 //     sx={{
