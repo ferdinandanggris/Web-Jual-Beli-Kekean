45bf9b035e51ba2f43e79612150dd24e24bea556
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-10 10:46:49 +0700

    changed the hero 1 to become a static image

--- resources/js/components/Hero.js
+++ resources/js/components/Hero.js
@@ -8,49 +8,13 @@ import Carousel from "react-material-ui-carousel";
 export default function Hero() {
     return (
         <Container sx={{ display: { mobile: "none", laptop: "block" }, mt: 3 }}>
-            <Carousel indicators={true} swipe={false} height={310}>
+            <Carousel indicators={true} swipe={false} height={275}>
                 <div className="hero">
                     <Box
                         sx={{ borderRadius: 1, width: "100%" }}
                         component="img"
-                        src="../images/hero.png"
+                        src="../images/hero-1.png"
                     />
-                    <Box
-                        sx={{
-                            position: "absolute",
-                            top: { laptop: "2rem", desktop: "1rem" },
-                            left: {
-                                laptop: "3rem",
-                                laptop: "7rem",
-                                desktop: "2rem",
-                            },
-                        }}
-                    >
-                        <Typography
-                            fontWeight={"medium"}
-                            fontSize={{
-                                tablet: 50,
-                                mobile: 13.8,
-                                laptop: 35,
-                                desktop: 40,
-                            }}
-                        >
-                            Batik Cakhra 1.1
-                        </Typography>
-                        <Typography
-                            fontWeight={600}
-                            color="subtitle"
-                            fontSize={{
-                                tablet: 15,
-                                mobile: 8.28,
-                                laptop: 15,
-                                desktop: 20,
-                            }}
-                        >
-                            Lorem ipsum dolor sit amet, consectetur
-                            <br /> adipiscing elit.{" "}
-                        </Typography>
-                    </Box>
                 </div>
                 <div className="hero">
                     <Box
