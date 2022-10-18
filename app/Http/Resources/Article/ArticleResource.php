07f90ae5dab1d67e90f0381a199101c9eeda1873
Farhan izzaz <farhanizzaz@gmail.com>
2022-10-18 16:26:50 +0700

    asdadfsdf

--- app/Http/Resources/Article/ArticleResource.php
+++ app/Http/Resources/Article/ArticleResource.php
@@ -15,6 +15,7 @@ class ArticleResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
             "title" => $this->title,
             "isi" => $this->isi,
             "featured" => $this->featured,
