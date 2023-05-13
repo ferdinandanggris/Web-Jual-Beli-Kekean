eb3a127b5add577a44c255512bd86b2936d6fd96
Farhan izzaz <farhanizzaz@gmail.com>
2023-05-13 07:09:10 +0700

    asd

--- resources/js/container/Model3d.jsx
+++ resources/js/container/Model3d.jsx
@@ -2,6 +2,7 @@ import { OrbitControls, PresentationControls, Stage } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import React from "react";
 import { Suspense } from "react";
+import { PerspectiveCamera } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import {environment} from  "../environments/environment"
 
